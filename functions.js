1ba3c9310f51a2b39fd4659f6bb7f579818d9f98
Your Name <you@example.com>
2020-02-26 15:40:53 -0800

    js

--- functions.js
+++ functions.js
@@ -2,15 +2,37 @@
 
 
 // 1. isHometown
-
+function isHometown(town){
+  return town ==='San Francisco';
+}
 // Define your function here
 
 
 // 2. getFullName
-
+function getFullName(first, last){
+  return `${first} ${last}`;
+}
 // Define your function here
 
 
 // 3. calculateTotal
-
+function calculateTotal (base, state, tax=0.05) {
+  let fee = 0;
+  let subtotal = base * (1 + tax);
+  if(state === 'CA'){
+    fee = 0.03* subtotal;
+  }
+  else if (state === 'PA') {
+    fee = 2;
+  }
+  else if ( state === 'MA') {
+    if ( base <= 100) {
+      fee = 1;
+    }
+    else (
+      fee = 3)
+  }
+  
+  return (subtotal + fee);
+}
 // Define your function here
--- objects.js
+++ objects.js
@@ -4,10 +4,38 @@
 // 1. countWords
 function countWords(phrase) {
   // Replace this with your code
+
+  let wordCount = {};
+
+  for(const word of phrase.split(' ')){
+    if (wordCount[word]){
+      wordCount[word] += 1;
+    } else { 
+     wordCount[word] = 1;
+   }
+
+  }
+ return wordCount; 
 }
 
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
   // Replace this with your code
+
+  const melon_prices = {
+      2.50: ['Cantaloupe', 'Honeydew'],
+      2.95: ['Watermelon'],
+      3.25: ['Musk', 'Crenshaw'],
+      14.25: ['Christmas']
+  }
+
+  if (melon_prices.hasOwnProperty(price)){
+    console.log(melon_prices[price].sort());
+  }
+  else {
+    console.log('none found');
+  }
+
 }
+
